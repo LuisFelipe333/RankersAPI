201737c4f651dad5a6082df61ec64510d6fee0ca
LuisFelipe333 <luigifelipe333@gmail.com>
2023-03-10 20:39:56 -0600

    Added Special Positions

--- RankersAPI/Models/Positions.cs
+++ RankersAPI/Models/Positions.cs
@@ -6,6 +6,13 @@
         SpearBearer = 1,
         Scout = 2,
         LightBearer = 3,
-        WaveController = 4
+        WaveController = 4,
+        Guide = 5,
+        Defender = 6,
+        Anima = 7,
+        Wonsulsa = 8,
+        Dansulsa = 9,
+        Hwayeomsa = 10,
+        Jeonsulsa = 11
     }
 }
--- RankersAPI/Models/Ranker.cs
+++ RankersAPI/Models/Ranker.cs
@@ -5,7 +5,7 @@
         public int rank {  get; set; }
         public string name { get; set; }
         public string sobriquet { get; set; }
-        public Positions position { get; set; }
+        public Positions[] positions { get; set; }
         public string description { get; set; }
     }
 }
