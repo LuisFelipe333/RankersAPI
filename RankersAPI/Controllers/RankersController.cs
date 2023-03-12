4c08a1e41d9763d5a45d95c09394f7eec32c5962
LuisFelipe333 <luigifelipe333@gmail.com>
2023-03-11 23:38:22 -0600

    Added Rankers Controller

---
+++ RankersAPI/Controllers/RankersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RankersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankersController : ControllerBase
+    {
+    }
+}
