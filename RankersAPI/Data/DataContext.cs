59daa1f942f1d3f002ce6d498fbb3d9ab44838c8
LuisFelipe333 <luigifelipe333@gmail.com>
2023-03-13 23:19:48 -0600

    Add DataContext to Program.cs

--- RankersAPI/Data/DataContext.cs
+++ RankersAPI/Data/DataContext.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RankersAPI.Models;
 
 namespace RankersAPI.Data
 {
     public class DataContext : DbContext
     {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Ranker> Rankers { get; set; }
+
     }
 }
