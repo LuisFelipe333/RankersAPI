fb6088295b8f7296283d73b64fc78c1329d9a734
LuisFelipe333 <luigifelipe333@gmail.com>
2023-03-13 23:56:25 -0600

    Add Migrations

---
+++ RankersAPI/Migrations/20230314055608_CreateInitial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RankersAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rankers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    rank = table.Column<int>(type: "int", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    sobriquet = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    positions = table.Column<int>(type: "int", nullable: false),
+                    secondaryPosition = table.Column<int>(type: "int", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rankers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rankers");
+        }
+    }
+}
--- RankersAPI/Models/Ranker.cs
+++ RankersAPI/Models/Ranker.cs
@@ -4,10 +4,12 @@ namespace RankersAPI.Models
 {
     public class Ranker
     {
+        public int Id { get; set; }
         public int rank {  get; set; }
-        public string name { get; set; }
+        public string name { get; set; } 
         public string sobriquet { get; set; }
-        public Positions[] positions { get; set; }
+        public Positions positions { get; set; }
+        public Positions secondaryPosition { get; set; }
         public string description { get; set; }
     }
 }
