44f3d2daf187ad0d1d8551e7ce80c7aa81384a1b
LuisFelipe333 <luigifelipe333@gmail.com>
2023-03-14 23:53:15 -0600

    Get Ranker by Id Added in method GetRankerById and HttpPost method Added

--- RankersAPI/Controllers/RankersController.cs
+++ RankersAPI/Controllers/RankersController.cs
@@ -11,9 +11,27 @@ namespace RankersAPI.Controllers
         private readonly DataContext context;
 
         [HttpGet]
-        public async Task<ActionResult<List<Ranker>>> GetRankers()
+        public async Task<ActionResult<List<Ranker>>> GetListRankers()
         {
             return Ok(await context.Rankers.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ranker>> GetRankerById(int id)
+        {
+            var ranker = await context.Rankers.FindAsync(id);
+            if(ranker == null)
+                return BadRequest("Ranker not found");
+            return Ok(ranker);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Ranker>>> AddRanker(Ranker ranker)
+        {
+            context.Rankers.Add(ranker);
+            await context.SaveChangesAsync();
+
+            return Ok(await context.Rankers.ToListAsync());
         }
 
         public RankersController(DataContext context)
